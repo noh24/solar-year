8413c0356b12a05428b57c002ace46fef6fffcb7
Brian Noh <noh24@ymail.com>
2022-11-14 11:57:01 -0500

    Add solar age calc object test

--- __tests__/solar-year.test.js
+++ __tests__/solar-year.test.js
@@ -1,9 +1,25 @@
-// import Whatever from ./path
 
-// describe('whatever whatever', () => {
-  
-//   test('should correctly test for whatever you want', () => {
-//     let code = whatever();
-//     expect(whatever()).toEqual('whatever');
-//   });
-// });
+import SolarAgeCalc from './../src/js/solar-year.js'
+
+describe('SolarAgeCalc', () => {
+
+  test('should create SolarAgeCalc object with 3 properties', () => {
+    let solarAgeCalc = new SolarAgeCalc("john", 23);
+    solarAgeCalc;
+    expect(solarAgeCalc.name).toEqual("john");
+    expect(solarAgeCalc.earthAge).toEqual(23);
+    expect(solarAgeCalc.lifeExpectancy).toEqual(0);
+  });
+});
+
+//get users earth years
+  // mercury .24
+  // venus .62
+  // mars 1.88
+  // jupiter 11.86
+  // life expectancy
+    // country of residence
+    // average steps per day
+    // stress level
+  // left years
+  // over years
