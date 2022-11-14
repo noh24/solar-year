cb95a4987b18c83ef6a0ab8bea33ec23c08414b9
Brian Noh <noh24@ymail.com>
2022-11-14 17:32:53 -0500

    wip: add form submission function

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,29 @@
-//  import example
-// import { Whatever } from './example-for-export.js';
+import SolarAgeCalc from './../src/js/solar-year.js';
 
-// import example for export default
-// import Whatever from './example-for-export.js'
-// import './../css/styles.css';
+window.addEventListener('load', function() {
+  document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    let solarAgeCalc;
+    solarAgeCalc = handleFormSubmission();
+  })
+});
+
+function handleFormSubmission() {
+  let name = document.getElementById('name').value;
+  let age = document.getElementById('age').value;
+  let religious = document.querySelector('input[name=religious]:checked').id;
+  let steps = document.getElementById('steps').value;
+  let stressLevel = document.querySelector('input[name=stress]:checked').id;
+  
+  let solarAgeCalc = new SolarAgeCalc(name, age);
+  solarAgeCalc.ageInPlanetYears();
+  lifeExpectIfUserHasReligion(religious);
+  lifeExpectBasedOnSteps(steps);
+  lifeExpectBasedOnStressLevel(stressLevel);
+  
+  return solarAgeCalc;
+}
+
+function displayContent() {
+  
+}
